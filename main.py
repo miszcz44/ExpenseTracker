109c7da358f716c329ece92990a7d0d1d38043bf
Eryk Bojaruniec <erykb412@wp.pl>
2022-05-30 12:19:24 +0200

    Dodano mozliwosc dodawania wydatku, pozostaje sortowanie kategorii malejaco

---
+++ main.py
@@ -0,0 +1,57 @@
+import json
+
+def Show_expenses():
+    print("Jedzenie: " + str(Categories[0]["jedzenie"]))
+    print("Transport: " + str(Categories[0]["transport"]))
+    print("Alkohol: " + str(Categories[0]["alkohol"]))
+    print("Rozrywka: " + str(Categories[0]["rozrywka"]))
+    print("Edukacja: " + str(Categories[0]["edukacja"]))
+    print("Inne: " + str(Categories[0]["inne"]))
+
+def Add_expense(user_choice2):
+    print("Wydatek:")
+    expense = float(input())
+    category = "fdas"
+    print(user_choice2)
+    if(int(user_choice2) == 1): category = "jedzenie"
+    elif(int(user_choice2) == 2): category = "transport"
+    elif(int(user_choice2) == 3): category = "alkohol"
+    elif(int(user_choice2) == 4): category = "rozrywka"
+    elif(int(user_choice2) == 5): category = "edukacja"
+    elif(int(user_choice2) == 6): category = "inne"
+    Categories[0][category] += expense
+    with open("Categories.json", "w") as json_file:
+        json.dump(Categories, json_file)
+
+second_menu_possibilities = [1, 2, 3, 4, 5, 6]
+
+with open("Categories.json", "r") as json_file:
+    Categories = json.load(json_file)
+
+
+
+
+
+
+while True:
+    print("1.Dodaj wydatek\n2.Wyświetl wydatki według kategorii\n3.Wyjdź")
+    user_choice = input()
+    if(int(user_choice) == 3):
+        break
+    elif(int(user_choice) == 1):
+        while(True):
+            print("1.Jedzenie\n2.Transport\n3.Alkohol\n4.Rozrywka\n5.Edukacja\n6.Inne\n7.Wróć")
+            user_choice2 = input()
+            if(int(user_choice2) == 7):
+                break
+            elif(int(user_choice2) in second_menu_possibilities):
+                Add_expense(user_choice2)
+            else:
+                print("Błąd")
+                continue
+    elif(int(user_choice) == 2):
+        Show_expenses()
+    else:
+        print("Błąd")
+        continue
+
